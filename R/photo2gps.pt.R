90adebe36e078fe736f57f8a780064f37e59f971
Guofang Liu <liugf@ibcas.ac.cn>
2018-07-31 22:50:53 +0800

    fix bug when second is zero.

--- R/photo2gps.pt.R
+++ R/photo2gps.pt.R
@@ -30,11 +30,19 @@ photo2gps.pt <- function(photo.pos, jhead.pos){
 			res[i,] = c(file.name[i],out[[i]][grep("GPS L",out[[i]])])
 	}
 	
+	transform.fun <- function(x){
+		0.8 + 3.5762788996862587e-06*x # fix bug when second is zero.
+	}
+
+
 	for(j in 2:3)
 		for(i in 1:nrow(res)){
 			if(!is.na(res[i,j]) & regexpr('[0-9]',res[i,j])!=-1)  {
-				pos <- gregexpr("[0-9]{1,5}",res[i,j])
-				res[i,j] = as.numeric(regmatches(res[i,j],pos)[[1]]) %*% c(1,1/60,1/3600,1/100/3600)
+				pos <- gregexpr("[0-9]{1,30}",res[i,j])
+				value <- as.numeric(regmatches(res[i, j], pos)[[1]]) 
+				if(length(grep("7fd",res[i,j]))==0) 
+					res[i, j] = value %*% c(1, 1/60, 1/3600, 1/100/3600) else 
+				res[i, j] = c(value[1:2],transform.fun(value[3]),value[4]) %*%  c(0, 1, 1/3600, 0)
 			} else res[i,j]=''
 		}
 	res <- data.frame(res, stringsAsFactors=FALSE)
