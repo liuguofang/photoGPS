89b8e4de1fb3859ef44e347c11fd044fa6bd9de9
Guofang Liu <liuguofang2004@126.com>
2019-08-01 17:13:24 +0800

    Update GPS.pt2photo.R

--- R/GPS.pt2photo.R
+++ R/GPS.pt2photo.R
@@ -4,7 +4,7 @@ GPS.pt2photo <- function(photo.pos ="d:/photo", exiftool.pos= "D:/software/EXE",
     file.copy(sprintf("%s/%s", exiftool.pos, "exiftool.exe"), 
         getwd())
     file <- dir(pattern = ".jpg|.JPG")
-    cmd <- sprintf("exiftool -GPSLatitude=%f -GPSLongitude=%f %s",GPS.point[2],GPS.point[1],file)
+    cmd <- sprintf("exiftool -GPSLatitude=%f -GPSLongitude=%f \"%s\"",GPS.point[2],GPS.point[1],file)
 
     lapply(cmd, shell)
 }
